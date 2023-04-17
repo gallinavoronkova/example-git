9386c2a7ac1e4a78d4f890d279601fdc0e5602b9
H_Voronkova <gallinavoronkova@gmail.com>
2023-04-17 14:30:11 +0200

    added gitignore with teacher

---
+++ src/main/java/com/examplegit/examplegit/ExampleGitApplication.java
@@ -0,0 +1,13 @@
+package com.examplegit.examplegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitApplication.class, args);
+	}
+
+}
